e4fc4e98181b68574a022ed886de299b0958d0a2
info-arnav <60637717+info-arnav@users.noreply.github.com>
2020-04-29 12:34:48 +0530

    hoosks

--- searchParams.js
+++ searchParams.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 export const searchParams = () => {
-  const [location, setLocation] = useState("Seattle");
+  const [location, setLocation] = useState("Seattle"); //suse state is as hook in hich oation sis sssseattle and setlocations ca  change the location hen called
   return (
     <div className="search-params">
       <h1>{location}</h1>
