955ca8971ef52c74c91eda1a00d640526fc8df28
info-arnav <60637717+info-arnav@users.noreply.github.com>
2020-04-29 14:31:13 +0530

    animals

--- searchParams.js
+++ searchParams.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 export const searchParams = () => {
+  const changableArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [array, changeValue] = useState(changableArray);
   const [location, setLocation] = useState("Seattle"); //suse state is as hook in hich oation sis sssseattle and setlocations ca  change the location hen called
   return (
     <div className="search-params">
@@ -15,6 +17,20 @@ export const searchParams = () => {
               setLocation(event.target.value);
             }}
           ></input>
+        </label>
+        <label htmlFor="pet">
+          pet
+          <select
+            placeholder="Pet"
+            id="pet"
+            onChange={(event) => changeValue(event.target.value)}
+            onBlur={(event) => changeValue(event.target.value)}
+          >
+            <option>All</option>
+            {changableArray.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
           <button>Submit</button>
         </label>
       </form>
