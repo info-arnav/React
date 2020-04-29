ba9155bd42e9bdf9259012c0d7aa99ae899bc5f0
info-arnav <60637717+info-arnav@users.noreply.github.com>
2020-04-29 22:37:47 +0530

    update

---
+++ useDrop.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+const useDrop = (name, defaultValue, array) => {
+  const [mainValue, changer] = useState(defaultValue);
+  const key = { name };
+  const creator = () => (
+    <label htmlFor={key}>
+      {name}
+      <select
+        id={key}
+        placeholder="all"
+        value={mainValue}
+        onChange={(event) => changer(event.target.value)}
+        onBlur={(event) => changer(event.target.value)}
+      >
+        <option>all</option>
+        {array.map((keyValue) => (
+          <option key={keyValue} value={keyValue}>
+            {keyValue}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+  return [mainValue, changer, creator];
+};
+
+export default useDrop;
